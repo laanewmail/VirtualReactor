bf6fb82bfa8d70c48131db985e88255899683732
laanewmail <laanewmail@mail.ru>
2021-09-07 21:40:41 +0300

    Добавили реализацию численного решения

--- Solvers/numeric_solve.py
+++ Solvers/numeric_solve.py
@@ -22,6 +22,21 @@ class NumericSolution(Base_Class_For_all):
     def nf_Solve(self, y0=None, t=None, max_step=0.1):  # y0 - начальные условия
         return solve_ivp(self.odesystem, t if t else self.t, y0 if y0 else self.y0 , max_step=max_step)
 
+    def arrenius(self, k0):
+            return k0 * (math.e) ** (-self.E / (self.R * self.T))
+
+    # #TODO: дорисовать про константу аррениуса
+    # k0 = 1
+    # T = np.linspace(300, 500, 100, dtype=int)
+    # R = 8.314
+    # E = 50
+    #
+    # def arrenius(T, k0, E):
+    #     return k0 * (math.e) ** (-E / (R * T))
+    #
+    # plt.plot(T, arrenius(T, k0, E))
+    # plt.show()
+
     def plot_Yi_t(self, t, yi):
         pylab.plot(t, yi)
 
