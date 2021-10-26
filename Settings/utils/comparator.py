56bb718cbee12c2a5ee0587fbb8d12dcb7c67e90
laanewmail <laanewmail@mail.ru>
2021-10-26 22:15:21 +0300

    Реализовано вычисление невязки по диапазонам значнеий к1 и к2

--- Settings/utils/comparator.py
+++ Settings/utils/comparator.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from Solvers.analit_solve import AnalitSolution
 from Solvers.numeric_solve import NumericSolution
 
@@ -14,7 +16,10 @@ class Comparator:
         self.analit_solver = AnalitSolution()
         self.numeric_solver = NumericSolution(time=self.analit_solver.t)
 
-    def calculate(self, k1: float = None, k2: float = None):
+    def _get_time_indexes(self):
+        return [tuple(self.numeric_solver.t).index(t) for t in self.analit_solver.rnd_time]
+
+    def calculate_single_k(self, k1: float = None, k2: float = None):
         self.numeric_solver.k1 = k1 if k1 is not None else self.numeric_solver.k1
         self.numeric_solver.k2 = k2 if k2 is not None else self.numeric_solver.k2
 
@@ -25,12 +30,55 @@ class Comparator:
         self.analit_solver.k1 = self.numeric_solver.k1
         self.analit_solver.k2 = self.numeric_solver.k2
 
-        tmp_experiment_results = [self.analit_solver.rnd_points(our_function=func, norm=norm)
+        self.experiment_results = [self.analit_solver.rnd_points(our_function=func, norm=norm)
                                   for func, norm in [(self.analit_solver.SolveY1, 1.0),
                                                      (self.analit_solver.SolveY3, 2.5),
                                                      (self.analit_solver.SolveY5, 18.0)]
                                   ]
-        # self.experiment_results = dict(zip(
-        #     [t for t,_ in ], []
-        # ))
 
+        tmp_numeric_solution = []
+        rnd_time_indexes = self._get_time_indexes()
+        for y in self.numeric_solution:
+            tmp_numeric_solution.append([y[i] for i in rnd_time_indexes])
+
+        shift = []
+        for i, n_solution in enumerate(tmp_numeric_solution):
+            shift.append(abs(np.array(n_solution) - np.array(self.experiment_results[i][1])))
+
+        return sum(sum(shift))
+
+    def calculate_range_k(self, k1_range, k2_range):
+        min_shift = {'k1': self.numeric_solver.k1,
+                     'k2': self.numeric_solver.k2,
+                     'shift': self.calculate_single_k(self.numeric_solver.k1, self.numeric_solver.k2)}
+        print(f"K:({min_shift['k1']}, {min_shift['k2']}) ;  SHIFT: {min_shift['shift']}")
+
+        matching_shifts = []
+
+        for k1 in k1_range:
+            for k2 in k2_range:
+                curr_shift = {'k1': k1,
+                              'k2': k2,
+                              'shift': self.calculate_single_k(k1, k2)}
+                print(f"K:({curr_shift['k1']}, {curr_shift['k2']}) ;  SHIFT: {curr_shift['shift']}")
+
+                if curr_shift['shift'] < min_shift['shift']:
+                    min_shift = curr_shift
+                    matching_shifts = []
+                elif curr_shift['shift'] == min_shift['shift']:
+                    matching_shifts.append(curr_shift)
+
+        print(f"MIN SHIFT: {min_shift}")
+        print(f"MATCHING_SHIFTS: {matching_shifts}")
+
+
+comparator = Comparator()
+
+k1_range = np.linspace(comparator.numeric_solver.k1 - comparator.numeric_solver.k1 * 0.999,
+                       comparator.numeric_solver.k1 + comparator.numeric_solver.k1 * 0.999,
+                       100)
+k2_range = np.linspace(comparator.numeric_solver.k2 - comparator.numeric_solver.k2 * 0.999,
+                       comparator.numeric_solver.k2 + comparator.numeric_solver.k2 * 0.999,
+                       100)
+
+comparator.calculate_range_k(k1_range, k2_range)
