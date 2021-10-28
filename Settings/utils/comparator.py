82ec1c0802caa0fc28554b53a9f86dfdfc920280
laanewmail <laanewmail@mail.ru>
2021-10-28 22:10:28 +0300

    Реализовано вычисление невязки по диапазонам значнеий к1 и к2

--- Settings/utils/comparator.py
+++ Settings/utils/comparator.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pylab
+from copy import deepcopy
 
 from Solvers.analit_solve import AnalitSolution
 from Solvers.numeric_solve import NumericSolution
@@ -19,22 +21,18 @@ class Comparator:
     def _get_time_indexes(self):
         return [tuple(self.numeric_solver.t).index(t) for t in self.analit_solver.rnd_time]
 
+    def generate_experiment_results(self):
+        self.experiment_results = [self.analit_solver.rnd_points(our_function=func, norm=norm)
+                                   for func, norm in [(self.analit_solver.SolveY1, 1.0),
+                                                      (self.analit_solver.SolveY3, 2.5),
+                                                      (self.analit_solver.SolveY5, 18.0)]
+                                   ]
+
     def calculate_single_k(self, k1: float = None, k2: float = None):
         self.numeric_solver.k1 = k1 if k1 is not None else self.numeric_solver.k1
         self.numeric_solver.k2 = k2 if k2 is not None else self.numeric_solver.k2
 
         self.numeric_solution = self.numeric_solver.nf_Solve()
-
-        # TODO: в общем случае аналитическое решение для шума не используется,
-        #  такая реализация временная для демоснтрации
-        self.analit_solver.k1 = self.numeric_solver.k1
-        self.analit_solver.k2 = self.numeric_solver.k2
-
-        self.experiment_results = [self.analit_solver.rnd_points(our_function=func, norm=norm)
-                                  for func, norm in [(self.analit_solver.SolveY1, 1.0),
-                                                     (self.analit_solver.SolveY3, 2.5),
-                                                     (self.analit_solver.SolveY5, 18.0)]
-                                  ]
 
         tmp_numeric_solution = []
         rnd_time_indexes = self._get_time_indexes()
@@ -45,12 +43,12 @@ class Comparator:
         for i, n_solution in enumerate(tmp_numeric_solution):
             shift.append(abs(np.array(n_solution) - np.array(self.experiment_results[i][1])))
 
-        return sum(sum(shift))
+        return {'shift': sum(sum(shift)), 'numeric_solution': deepcopy(self.numeric_solution)}
 
     def calculate_range_k(self, k1_range, k2_range):
         min_shift = {'k1': self.numeric_solver.k1,
                      'k2': self.numeric_solver.k2,
-                     'shift': self.calculate_single_k(self.numeric_solver.k1, self.numeric_solver.k2)}
+                     **self.calculate_single_k(self.numeric_solver.k1, self.numeric_solver.k2)}
         print(f"K:({min_shift['k1']}, {min_shift['k2']}) ;  SHIFT: {min_shift['shift']}")
 
         matching_shifts = []
@@ -59,7 +57,7 @@ class Comparator:
             for k2 in k2_range:
                 curr_shift = {'k1': k1,
                               'k2': k2,
-                              'shift': self.calculate_single_k(k1, k2)}
+                              **self.calculate_single_k(k1, k2)}
                 print(f"K:({curr_shift['k1']}, {curr_shift['k2']}) ;  SHIFT: {curr_shift['shift']}")
 
                 if curr_shift['shift'] < min_shift['shift']:
@@ -70,9 +68,33 @@ class Comparator:
 
         print(f"MIN SHIFT: {min_shift}")
         print(f"MATCHING_SHIFTS: {matching_shifts}")
+        self._plot_numeric_solution_with_rnd_points(min_shift)
+
+    def _plot_numeric_solution_with_rnd_points(self, shift_obj):
+        # pylab.ion()
+        pylab.subplot(1, 3, 1)
+        pylab.title(f"K1: {shift_obj['k1']}, K2: {shift_obj['k2']}")
+        pylab.plot(self.numeric_solver.t, shift_obj['numeric_solution'][0] / max(shift_obj['numeric_solution'][0]))
+        pylab.scatter(x=self.experiment_results[0][0], y=self.experiment_results[0][1], marker='o', c='r',
+                      edgecolor='b')
+
+        pylab.subplot(1, 3, 2)
+        pylab.plot(self.numeric_solver.t, shift_obj['numeric_solution'][1] / max(shift_obj['numeric_solution'][1]))
+        pylab.scatter(x=self.experiment_results[1][0], y=self.experiment_results[1][1], marker='o', c='r',
+                      edgecolor='b')
+
+        pylab.subplot(1, 3, 3)
+        pylab.plot(self.numeric_solver.t, shift_obj['numeric_solution'][2] / max(shift_obj['numeric_solution'][2]))
+        pylab.scatter(x=self.experiment_results[2][0], y=self.experiment_results[2][1], marker='o', c='r',
+                      edgecolor='b')
+        pylab.show()
+        # pylab.pause(1)
+        # pylab.close()
 
 
 comparator = Comparator()
+
+comparator.generate_experiment_results()
 
 k1_range = np.linspace(comparator.numeric_solver.k1 - comparator.numeric_solver.k1 * 0.999,
                        comparator.numeric_solver.k1 + comparator.numeric_solver.k1 * 0.999,
