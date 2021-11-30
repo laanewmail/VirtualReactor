823f3f375d2cd6fdb5d94091cc7faf82422593c9
laanewmail <laanewmail@mail.ru>
2021-11-30 22:05:50 +0300

    Добавили: - .gitignore - генерирование рез-ов эксперимента в Численном решении - поправили плоттер

--- Settings/utils/plotter.py
+++ Settings/utils/plotter.py
@@ -8,48 +8,47 @@ import pylab
 def plot_all(**kwargs):
     A_Solver = AnalitSolution()
 
-    N_Solver = NumericSolution(time=A_Solver.t)
+    N_Solver = NumericSolution()
 
     N_Solution = N_Solver.nf_Solve()
+    N_Solver.generate_rnd_time()
+    rnd_N_Solution = N_Solver.rnd_points(N_Solution)
 
     # SOLVE 1
     pylab.subplot(3, 3, 1)
     pylab.title("Численное решение")
     pylab.plot(N_Solver.t, N_Solution[0])
 
-    pylab.subplot(3, 3, 2)
+    pylab.subplot(3, 3, 3)
     pylab.title("Аналитическое решение")
     a_solve_1 = A_Solver.SolveY1(A_Solver.t)
     pylab.plot(A_Solver.t, a_solve_1/max(a_solve_1))
 
-    pylab.subplot(3, 3, 3)
+    pylab.subplot(3, 3, 2)
     pylab.title("Экспериментальные данные")
-    points = A_Solver.rnd_points(A_Solver.SolveY1)
-    pylab.scatter(x=points[0], y=points[1], marker='o', c='r', edgecolor='b')
+    pylab.scatter(x=rnd_N_Solution[0], y=rnd_N_Solution[1][0], marker='o', c='r', edgecolor='b')
 
     # SOLVE 2
     pylab.subplot(3, 3, 4)
     pylab.plot(N_Solver.t, N_Solution[1])
 
-    pylab.subplot(3, 3, 5)
+    pylab.subplot(3, 3, 6)
     a_solve_3 = A_Solver.SolveY3(A_Solver.t)
     pylab.plot(A_Solver.t,  a_solve_3 / max(a_solve_3))
 
-    pylab.subplot(3, 3, 6)
-    points = A_Solver.rnd_points(A_Solver.SolveY3)
-    pylab.scatter(x=points[0], y=points[1], marker='o', c='r', edgecolor='b')
+    pylab.subplot(3, 3, 5)
+    pylab.scatter(x=rnd_N_Solution[0], y=rnd_N_Solution[1][1], marker='o', c='r', edgecolor='b')
 
     # SOLVE 3
     pylab.subplot(3, 3, 7)
     pylab.plot(N_Solver.t, N_Solution[2]/9)
 
-    pylab.subplot(3, 3, 8)
+    pylab.subplot(3, 3, 9)
     a_solve_5 = A_Solver.SolveY5(A_Solver.t)
     pylab.plot(A_Solver.t, a_solve_5 / max(a_solve_5))
 
-    pylab.subplot(3, 3, 9)
-    points = A_Solver.rnd_points(A_Solver.SolveY5)
-    pylab.scatter(x=points[0], y=points[1], marker='o', c='r', edgecolor='b')
+    pylab.subplot(3, 3, 8)
+    pylab.scatter(x=rnd_N_Solution[0], y=rnd_N_Solution[1][2], marker='o', c='r', edgecolor='b')
 
     pylab.show()
 
--- Solvers/numeric_solve.py
+++ Solvers/numeric_solve.py
@@ -1,4 +1,7 @@
-import pylab
+from random import randint as rnd
+from random import random
+
+import numpy as np
 
 from Solvers.base_class import *
 
@@ -8,8 +11,9 @@ class NumericSolution(BaseClassForAll):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.y0 = [1, 1, 0]
+        self.generate_rnd_time()
 
-    def odesystem(self, y,  t):  # , params
+    def odesystem(self, y, t):  # , params
         y1, y3, y5 = y
         # y   z    x
         return [
@@ -18,14 +22,14 @@ class NumericSolution(BaseClassForAll):
             3 * self.k2 * y3 * y3
         ]
 
-    #def nf_Solve(self, y0=None, t=None, max_step=2):  # y0 - начальные условия
+    # def nf_Solve(self, y0=None, t=None, max_step=2):  # y0 - начальные условия
     #   return solve_ivp(self.odesystem, t if t else self.t, y0 if y0 else self.y0 , max_step=max_step)
 
-    def nf_Solve(self,  y0=None, t=None):  # y0 - начальные условия
+    def nf_Solve(self, y0=None, t=None):  # y0 - начальные условия
         return odeint(self.odesystem, y0 if y0 else self.y0, t if t else self.t).T
 
     def arrenius(self, k0):
-            return k0 * (math.e) ** (-self.E / (self.R * self.T))
+        return k0 * (math.e) ** (-self.E / (self.R * self.T))
 
     # #TODO: дорисовать про константу аррениуса
     # k0 = 1
@@ -42,6 +46,37 @@ class NumericSolution(BaseClassForAll):
     def plot_Yi_t(self, t, yi):
         pylab.plot(t, yi)
 
+    def generate_rnd_time(self):
+        x = []
+        for i in range(default_config.get("rnd_points_number", 50)):
+            curr_n = self.t[rnd(0, len(self.t) - 1)]
+            while curr_n in x:
+                curr_n = self.t[rnd(0, len(self.t) - 1)]
+            x.append(curr_n)
+        x.sort()
+        self.rnd_time = x
+
+    def _get_time_indexes(self):
+        return [tuple(self.t).index(time) for time in self.rnd_time]
+
+    def rnd_points(self, num_solution):
+
+        rnd_indexes = self._get_time_indexes()
+        result = []
+        for solution in num_solution:
+            solution_delta = []
+            for index in rnd_indexes:
+                delta = solution[index] * (random() - 0.5) / max(solution)
+                val = solution[index] + delta
+                solution_delta.append(val)
+            max_val = max(solution_delta)
+            solution_delta = [obj/max_val for obj in solution_delta]
+            result.append(solution_delta)
+        return np.array(self.rnd_time), result
+
+        # max_val = max([our_function(x) for x in self.t])
+        # return np.array(self.rnd_time), [(our_function(xi) + our_function(xi) * (random() - 0.5) / max_val) / max_val
+        #                                  for xi in self.rnd_time]
 
 # solution = NumericSolution()
 # res = solution.nf_Solve()
@@ -50,4 +85,3 @@ class NumericSolution(BaseClassForAll):
 # solution.plot_Yi_t(res.t, res.y[2]/9)
 #
 # pylab.show()
-
