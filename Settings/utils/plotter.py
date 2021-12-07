50b4618e97f3f483223e5f322427d117785c1338
laanewmail <laanewmail@mail.ru>
2021-12-07 21:15:20 +0300

    Поправили plotter.py

--- Settings/utils/plotter.py
+++ Settings/utils/plotter.py
@@ -1,13 +1,9 @@
-from Solvers.analit_solve import AnalitSolution
-from Solvers.numeric_solve import NumericSolution
-
-import matplotlib.pyplot as plt
 import pylab
+
+from Solvers.numeric_solve import NumericSolution
 
 
 def plot_all(**kwargs):
-    A_Solver = AnalitSolution()
-
     N_Solver = NumericSolution()
 
     N_Solution = N_Solver.nf_Solve()
@@ -17,12 +13,12 @@ def plot_all(**kwargs):
     # SOLVE 1
     pylab.subplot(3, 3, 1)
     pylab.title("Численное решение")
-    pylab.plot(N_Solver.t, N_Solution[0])
+    pylab.plot(N_Solver.t, N_Solution[0] / max(N_Solution[0]))
 
     pylab.subplot(3, 3, 3)
-    pylab.title("Аналитическое решение")
-    a_solve_1 = A_Solver.SolveY1(A_Solver.t)
-    pylab.plot(A_Solver.t, a_solve_1/max(a_solve_1))
+    pylab.title("Численное решение + Шум")
+    pylab.plot(N_Solver.t, N_Solution[0] / max(N_Solution[0]))
+    pylab.scatter(x=rnd_N_Solution[0], y=rnd_N_Solution[1][0], marker='o', c='r', edgecolor='b')
 
     pylab.subplot(3, 3, 2)
     pylab.title("Экспериментальные данные")
@@ -30,22 +26,22 @@ def plot_all(**kwargs):
 
     # SOLVE 2
     pylab.subplot(3, 3, 4)
-    pylab.plot(N_Solver.t, N_Solution[1])
+    pylab.plot(N_Solver.t, N_Solution[1] / max(N_Solution[1]))
 
     pylab.subplot(3, 3, 6)
-    a_solve_3 = A_Solver.SolveY3(A_Solver.t)
-    pylab.plot(A_Solver.t,  a_solve_3 / max(a_solve_3))
+    pylab.plot(N_Solver.t, N_Solution[1] / max(N_Solution[1]))
+    pylab.scatter(x=rnd_N_Solution[0], y=rnd_N_Solution[1][1], marker='o', c='r', edgecolor='b')
 
     pylab.subplot(3, 3, 5)
     pylab.scatter(x=rnd_N_Solution[0], y=rnd_N_Solution[1][1], marker='o', c='r', edgecolor='b')
 
     # SOLVE 3
     pylab.subplot(3, 3, 7)
-    pylab.plot(N_Solver.t, N_Solution[2]/9)
+    pylab.plot(N_Solver.t, N_Solution[2] / max(N_Solution[2]))
 
     pylab.subplot(3, 3, 9)
-    a_solve_5 = A_Solver.SolveY5(A_Solver.t)
-    pylab.plot(A_Solver.t, a_solve_5 / max(a_solve_5))
+    pylab.plot(N_Solver.t, N_Solution[2] / max(N_Solution[2]))
+    pylab.scatter(x=rnd_N_Solution[0], y=rnd_N_Solution[1][2], marker='o', c='r', edgecolor='b')
 
     pylab.subplot(3, 3, 8)
     pylab.scatter(x=rnd_N_Solution[0], y=rnd_N_Solution[1][2], marker='o', c='r', edgecolor='b')
